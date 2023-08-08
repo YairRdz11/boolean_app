c7dd3e0f912a7a843dc0b6aaaf4293c66aa253da
Yair Rfz <yairrdz11@gmail.com>
2023-08-07 22:07:06 -0600

    Creation table static

--- lib/src/pages/table_page.dart
+++ lib/src/pages/table_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 class TablePage extends StatefulWidget {
   final String args;
@@ -14,7 +16,7 @@ class _TablePageState extends State<TablePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Tabla')),
-      body: DataTable(
+      body: _createTable(widget.args),/*DataTable(
       columns: <DataColumn>[
         DataColumn(
           label: Expanded(
@@ -64,7 +66,7 @@ class _TablePageState extends State<TablePage> {
           ],
         ),
       ],
-    ),
+    ),*/
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context);
@@ -72,5 +74,59 @@ class _TablePageState extends State<TablePage> {
         child: const Icon(Icons.arrow_forward),
       )
     );
+  }
+
+  DataTable _createTable(String varibleNumber) {
+    int number = int.tryParse(varibleNumber) ?? 0;
+    return DataTable(
+      columns: _createColumns(number), 
+      rows: _createRows(number)
+    );
+  }
+  
+  List<DataColumn> _createColumns(int varibleNumber) {
+    List<DataColumn> columns = List.empty(growable: true);
+    DataColumn columnResult =  const DataColumn(
+          label: Expanded(
+            child: Text(
+              'f(x)',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        );
+    int character = "z".codeUnitAt(0);
+    for (var i = 0; i < varibleNumber; i++) {
+      String currentCharacter = String.fromCharCode(character);
+      DataColumn column = DataColumn(
+          label: Expanded(
+            child: Text(
+              '$currentCharacter',
+              style: TextStyle(fontStyle: FontStyle.italic),
+            ),
+          ),
+        );
+      columns.add(column);
+      character--;
+    }
+    columns = columns.reversed.toList();
+    columns.add(columnResult);
+    return columns;
+  }
+  
+  List<DataRow> _createRows(int number) {
+    num length = pow(2, number);
+    List<DataRow> rows = List.empty(growable: true);
+    for (var i = 0; i < length; i++) {
+      List<DataCell> cellList = List.empty(growable: true);
+      for (var j = 0; j < number; j++) {
+        DataCell cell = DataCell(Text('0'));
+        cellList.add(cell);
+      }
+      cellList.add(const DataCell(Text('0')));
+      DataRow row = DataRow(cells: cellList);
+      rows.add(row);
+    }
+
+    return rows;
   }
 }
